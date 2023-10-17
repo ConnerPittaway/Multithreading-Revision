a7a3cd0ceaff9b8fa07caa2aa944d18316e12752
Conner Pittaway <57526209+ConnerPittaway@users.noreply.github.com>
2023-10-17 15:12:59 +0100

    Lesson 5 - Worker and Worker Controller Class, CVs, Unique Ptrs

--- Multithreading Revision/Multithreading Revision/main.cpp
+++ Multithreading Revision/Multithreading Revision/main.cpp
@@ -70,6 +70,97 @@ int Big()
 	return 0;
 }
 
+class WorkerController
+{
+public:
+	WorkerController(int workerCount) : lk{ mtx }, workerCount{ workerCount } {}
+	void SignalDone()
+	{
+		bool needsNotification = false;
+		{
+			std::lock_guard lk{ mtx };
+			++doneCount;
+			if (doneCount == workerCount)
+			{
+				cv.notify_one();
+			}
+		}
+	}
+
+	void WaitForAllDone()
+	{
+		cv.wait(lk, [this] {return doneCount == workerCount; });
+		doneCount = 0;
+	}
+
+private:
+	std::condition_variable cv;
+	std::mutex mtx; //Always Mutex with CV
+	std::unique_lock<std::mutex> lk;
+	int workerCount = 0;
+
+	//Shared Memory
+	int doneCount = 0;
+};
+
+class Worker
+{
+public:
+	Worker(WorkerController* pWorkerController)
+		:
+		pController{ pWorkerController },
+		thread{ &Worker::Run_, this }
+	{}
+
+	void SetJob(std::span<int> data, int *pOut)
+	{
+		{
+			std::lock_guard lk{ mtx };
+			input = data;
+			pOutput = pOut;
+		}
+		cv.notify_one();
+	}
+
+	void Kill()
+	{
+		{
+			std::lock_guard lk{ mtx };
+			terminate = true;
+		}
+		cv.notify_one();
+	}
+
+private:
+
+	void Run_()
+	{
+		std::unique_lock lk{ mtx };
+		while (true)
+		{
+			cv.wait(lk, [this] {return pOutput !=nullptr || terminate; });
+			if (terminate)
+			{
+				break;
+			}
+			ProcessData(input, *pOutput);
+			pOutput = nullptr;
+			input = {};
+			pController->SignalDone();
+		}
+	}
+
+	WorkerController* pController;
+	std::jthread thread;
+	std::condition_variable cv;
+	std::mutex mtx;
+
+	//Shared Memory
+	std::span<int> input;
+	int* pOutput = nullptr;
+	bool terminate = false;
+};
+
 int Small()
 {
 	auto datasets = GenerateData();
@@ -84,22 +175,34 @@ int Small()
 	Timer timer;
 	timer.Mark();
 
-	int total = 0;
-	std::vector<std::jthread> workers;
+	//Create Worker Threads
+	constexpr size_t workerCount = 4;
+	WorkerController workerController{ workerCount }; // Initialise Controller
+	std::vector<std::unique_ptr<Worker>> workerPtrs;
+	for (size_t i = 0; i < workerCount; i++)
+	{
+		workerPtrs.push_back(std::make_unique<Worker>(&workerController));
+	}
+
 	const auto subsetSize = DATASET_SIZE / 10'000;
 	for (size_t i = 0; i < DATASET_SIZE; i += subsetSize)
 	{
 		for (size_t j = 0; j < 4; j++)
 		{
-			workers.push_back(std::jthread{ ProcessData, std::span(&datasets[j][i], subsetSize), std::ref(sum[j].v)});
+			workerPtrs[j]->SetJob(std::span(&datasets[j][i], subsetSize), &sum[j].v);
+			//workers.push_back(std::jthread{ ProcessData, std::span(&datasets[j][i], subsetSize), std::ref(sum[j].v)});
 		}
-		workers.clear();
-		total = 0;
+		workerController.WaitForAllDone();
 	}
 	auto t = timer.Peek();
 
-	std::cout << "Result is " << total << std::endl;
+	std::cout << "Result is " << sum[0].v + sum[1].v + sum[2].v + sum[3].v << std::endl;
 	std::cout << "Processing the datasets took " << t << " seconds\n";
+
+	for (auto& w : workerPtrs)
+	{
+		w->Kill();
+	}
 
 	return 0;
 }
