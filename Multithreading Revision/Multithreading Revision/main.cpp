71d16f145bc183be671f6d7bca4dd1c60edb029d
Conner Pittaway <57526209+ConnerPittaway@users.noreply.github.com>
2023-10-20 17:25:01 +0100

    Lesson 10 - Task Queue Prep, Queued Worker and Worker Controller
    Worker pointers generated with std::ranges::generate with unique pointers

--- Multithreading Revision/Multithreading Revision/main.cpp
+++ Multithreading Revision/Multithreading Revision/main.cpp
@@ -245,6 +245,163 @@ private:
 	size_t numHeavyItems;
 };
 
+class WorkerControllerQueued
+{
+public:
+	WorkerControllerQueued() : lk{ mtx } {}
+	void SignalDone()
+	{
+		bool needsNotification = false;
+		{
+			std::lock_guard lk{ mtx };
+			++doneCount;
+			if (doneCount == WorkerCount)
+			{
+				needsNotification = true;
+
+			}
+		}
+
+		if (needsNotification)
+		{
+			cv.notify_one();
+		}
+	}
+
+	void WaitForAllDone()
+	{
+		cv.wait(lk, [this] {return doneCount == WorkerCount; });
+		doneCount = 0;
+	}
+
+	void SetChunk(std::span<const Task> chunk)
+	{
+		idx = 0;
+		currentChunk = chunk;
+	}
+
+	const Task* GetTask()
+	{
+		//std::lock_guard lock{ mtx };
+		const auto i = idx.fetch_add(1);
+		if (i >= ChunkSize)
+		{
+			return nullptr;
+		}
+		return &currentChunk[i];
+	}
+
+private:
+	std::condition_variable cv;
+	std::mutex mtx; //Always Mutex with CV
+	std::unique_lock<std::mutex> lk;
+	std::span<const Task> currentChunk;
+	//Shared Memory
+	int doneCount = 0;
+	std::atomic<size_t> idx = 0;
+};
+
+class WorkerQueued
+{
+public:
+	WorkerQueued(WorkerControllerQueued* pWorkerController)
+		:
+		pController{ pWorkerController },
+		thread{ &WorkerQueued::Run_, this }
+	{}
+	
+	void StartWork()
+	{
+		{
+			std::lock_guard lk{ mtx };
+			working = true;
+		}
+		cv.notify_one();
+	}
+
+	void Kill()
+	{
+		{
+			std::lock_guard lk{ mtx };
+			terminate = true;
+		}
+		cv.notify_one();
+	}
+
+	unsigned int GetResult() const
+	{
+		return accululation;
+	}
+
+	float GetJobWorkTime() const
+	{
+		return workTime;
+	}
+
+	size_t GetNumHeavy() const
+	{
+		return numHeavyItems;
+	}
+
+	~WorkerQueued()
+	{
+		Kill();
+	}
+
+private:
+	void ProcessData_()
+	{
+		numHeavyItems = 0;
+		while(auto pTask = pController->GetTask())
+		{
+			accululation += pTask->Process();
+
+			if constexpr (timingMeasurementEnabled)
+			{
+				numHeavyItems += pTask->heavy;
+			}
+		}
+	}
+
+	void Run_()
+	{
+		std::unique_lock lk{ mtx };
+		while (true)
+		{
+			Timer timer;
+			cv.wait(lk, [this] {return working || terminate; });
+			if (terminate)
+			{
+				break;
+			}
+
+			if constexpr (timingMeasurementEnabled)
+			{
+				timer.Mark();
+			}
+			ProcessData_();
+			if constexpr (timingMeasurementEnabled)
+			{
+				workTime = timer.Peek();
+			}
+
+			working = false;
+			pController->SignalDone();
+		}
+	}
+	WorkerControllerQueued* pController;
+	std::jthread thread;
+	std::condition_variable cv;
+	std::mutex mtx;
+
+	//Shared Memory
+	unsigned int accululation = 0;
+	bool terminate = false;
+	bool working = false;
+	float workTime = -1.f;
+	size_t numHeavyItems;
+};
+
 int Experiment(bool stacked)
 {
 	const auto chunks = [=]
@@ -263,9 +420,9 @@ int Experiment(bool stacked)
 	totalTime.Mark();
 
 	//Create Worker Threads
-	WorkerController workerController; //Initialise Controller
-	std::vector<std::unique_ptr<Worker>> workerPtrs(WorkerCount);
-	std::ranges::generate(workerPtrs, [&workerController] {return std::make_unique<Worker>(&workerController); });
+	WorkerControllerQueued workerController; //Initialise Controller
+	std::vector<std::unique_ptr<WorkerQueued>> workerPtrs(WorkerCount);
+	std::ranges::generate(workerPtrs, [&workerController] {return std::make_unique<WorkerQueued>(&workerController); });
 
 	std::vector<ChunkTimeInfo> timings;
 	timings.reserve(ChunkCount);
@@ -277,9 +434,12 @@ int Experiment(bool stacked)
 		{
 			chunkTimer.Mark();
 		}
-		for (size_t iSubset = 0; iSubset < WorkerCount; iSubset++)
+
+		workerController.SetChunk(chunk);
+		for (const auto& w : workerPtrs)
 		{
-			workerPtrs[iSubset]->SetJob(std::span{ &chunk[iSubset * SubsetSize], SubsetSize });
+			w->StartWork();
+			//workerPtrs[iSubset]->SetJob(std::span{ &chunk[iSubset * SubsetSize], SubsetSize });
 			//workerThreads.push_back(std::jthread{ ProcessData, std::span{&datasets[j][i], subsetSize}, std::ref(sum[j].i)});
 		}
 		workerController.WaitForAllDone();
